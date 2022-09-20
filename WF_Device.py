902a46273b8ef8a97aa9e5d613985d5992f9ec33
Florian Guggi <flo@flow-movement.at>
2022-09-20 05:53:28 +0200

    add documentation

--- WF_Device.py
+++ WF_Device.py
@@ -3,18 +3,30 @@ import sys
 import logging
 
 class WF_Device:
+    """ A wrapper for any waveform supported device.
+    Do not forget to call connect()
+    """
+
     def __init__(self):
-        if sys.platform.startswith("win"):
-            self._dwf = cdll.LoadLibrary("dwf.dll")
-        elif sys.platform.startswith("darwin"):
-            self._dwf = cdll.LoadLibrary("/Library/Frameworks/dwf.framework/dwf"):
-        else:
-            self._dwf = cdll.LoadLibrary("libdwf.so")
+        try:
+            if sys.platform.startswith("win"):
+                self._dwf = cdll.LoadLibrary("dwf.dll")
+            elif sys.platform.startswith("darwin"):
+                self._dwf = cdll.LoadLibrary("/Library/Frameworks/dwf.framework/dwf"):
+            else:
+                self._dwf = cdll.LoadLibrary("libdwf.so")
+        except OSError:
+            logging.fatal("Could not load library, is WaveForms installed?")
+            raise
         self._hdwf = c_int(0)
 
-    def connect(self):
+    def connect(self, device_index=-1):
+        """Attempts to connect to a waveform device.
+        Specifying the device index allows to connect to a specific one;
+        otherwise the first one that is detected is used.
+        """
         logging.debug("Aquiring device...")
-        self._dwf.FDwfDeviceOpen(c_int(-1), byref(self._hdwf))
+        self._dwf.FDwfDeviceOpen(c_int(device_index), byref(self._hdwf))
         if self._hdwf.value == 0:
             logging.error("Faild to open device")
             szerr = create_string_buffer(512)
